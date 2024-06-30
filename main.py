84b89b0d79a1e47dc83c7b8d26ea98077e12c60c
yousuf <120650817+umarumair@users.noreply.github.com>
2024-06-30 09:58:15 +0530

    adding post, delete end points and requirements file

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
 from typing import List
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 from models import Email
 
 app = FastAPI()
 
-
 db: List[Email] = [
-    Email(id=1,receiver_email="abc@a.com", subject = "hi", body_text="Hello World"),
-    Email(id=2,receiver_email="abc@ab.com", subject = "hi", body_text="Hello World"),
+    Email(receiver_email="demo@gmail.com", subject="Demo", body_text="Hello world, how are you"),
+    Email(receiver_email="test@outlook.com", subject="Test", body_text="Hello this is the body of email"),
 ]
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"REST API": "go to /api/emails to fetch emails", "endpoints": "go to /docs for interactive API docs"}
 
 
-@app.get("/emails")
+@app.get("/api/emails")
 async def fetch_emails():
     return db
 
 
+@app.post("/api/emails")
+async def add_email(email: Email):
+    db.append(email)
+    return {"message": "Email added", "id": email.id}
+
+
+@app.delete("/api/emails/{id}")
+async def delete_email(id: int):
+    for email in db:
+        if email.id == id:
+            db.remove(email)
+            return {"message": "Email removed", "id": email.id}
+    raise HTTPException(status_code=404, detail=f"Email with {id} not found")
--- models.py
+++ models.py
@@ -1,10 +1,17 @@
-from typing import Optional
+from typing import Optional, ClassVar
 
-from pydantic import BaseModel, EmailStr, PositiveInt
+from pydantic import BaseModel, EmailStr, Field
 
 
 class Email(BaseModel):
-    id: Optional[PositiveInt]
+    id: Optional[int] = Field(default_factory=lambda: Email._increment_id())
     receiver_email: EmailStr
     subject: str
     body_text: str
+
+    _current_id: ClassVar[int] = 0
+
+    @classmethod
+    def _increment_id(cls) -> int:
+        cls._current_id += 1
+        return cls._current_id
